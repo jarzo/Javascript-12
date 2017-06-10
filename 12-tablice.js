f2b80ae1d9c5b7cb7a0104dd896efe3545a57ad4
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 16:22:02 +0200

    commit

--- 12-tablice.js
+++ 12-tablice.js
@@ -35,3 +35,27 @@ for (var i = 0; i < imiona.length ; i++) {
 imiona.forEach(function (element, i){
 	console.log('Element nr ' + i + '=' +element);
 });
+
+console.log(imiona.join(" - ") ); //dołacz do kody po między wszystkimi elemętami
+
+/*imiona.sort()// w kolejności alfabetycznej */
+
+/*imiona.reverse()//odwraca kolejnoiść tablicy od końca do początku*/
+
+var imionaMeskie =['Robert', 'Mariusz' , 'Andrzej'];
+
+var zbiorImion = imionaMeskie.concat(imiona);//concat łączy tablice//
+
+console.log(zbiorImion);
+
+console.log(zbiorImion.indexOf("Mariusz"));
+
+console.log( Array.isArray(zbiorImion));//sprawdza czy to jest tablica
+
+console.log(zbiorImion.slice(2, 5));
+
+zbiorImion.splice(2, 3, "Julek",  "Krzyś");//sortuje od 2, wycina 3 elementy i wstawia julka i krzysia
+
+console.log(zbiorImion);
+
+//to string-całą tablice zamienia na stringi , i wyświetala po przecinku
