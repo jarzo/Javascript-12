27e0580151cd13d8c4fa2cfd97d3d38bec0ba51f
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 15:50:49 +0200

    tablica
    pop-usuwa ostatni element
    unshift- dodaje na samym początku

--- 12-tablice.js
+++ 12-tablice.js
@@ -1,1 +1,21 @@
 'use strict'
+//tablica to to samo co objekt tylko na indexy//
+
+var imiona = ['Monika' , 'Krystian' , 'Łukasz'];
+//console.log(imiona);
+
+imiona[3] = 'Tania';//zamiana imion 3 i 2//
+imiona[2] = 'Ela';
+
+imiona.push('Geralt');
+
+//console.log(imiona);
+
+imiona.pop();//usuwa ostatni element listy//
+imiona.pop();
+
+//console.log(imiona);
+
+console.log(imiona.unshift('Robert'));//unshift dodaje jako pierwszy//
+
+console.log(imiona);
